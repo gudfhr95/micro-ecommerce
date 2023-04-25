92620b97421be2cbc8831554a8d17cd954a44f3f
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-04-26 01:20:27 +0900

    chore: add kotest

--- terms/build.gradle.kts
+++ terms/build.gradle.kts
@@ -2,7 +2,4 @@ group = "com.microecommerce.terms"
 version = "0.0.1"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
