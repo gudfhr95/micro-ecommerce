91fad5ee01bfd72772e3700316f4630e5fe96d0b
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-04-26 00:26:21 +0900

    chore: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.6" apply false
     id("io.spring.dependency-management") version "1.1.0" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.2" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("io.gitlab.arturbosch.detekt").version("1.23.0-RC2")
     kotlin("jvm") version "1.8.20"
     kotlin("plugin.spring") version "1.8.20" apply false
 }
@@ -13,6 +14,10 @@ version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
     repositories {
         mavenCentral()
     }
@@ -26,5 +31,11 @@ allprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+
+    detekt {
+        config = files("$rootDir/detekt.yml")
+        buildUponDefaultConfig = true
+        allRules = false
     }
 }
