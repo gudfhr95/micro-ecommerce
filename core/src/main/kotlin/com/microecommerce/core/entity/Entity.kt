50a7f642bae3342ff177ebd2115f2d0801e13273
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-04-27 01:14:56 +0900

    refactor: refactor AbstractId

--- core/src/main/kotlin/com/microecommerce/core/entity/Entity.kt
+++ core/src/main/kotlin/com/microecommerce/core/entity/Entity.kt
@@ -1,3 +1,11 @@
 package com.microecommerce.core.entity
 
-open class Entity : IdentifiedDomainObject()
+import java.time.LocalDateTime
+
+open class Entity(
+    protected val id: Long? = null,
+    protected val createdAt: LocalDateTime? = null,
+    protected val createdBy: String? = null,
+    protected val updatedAt: LocalDateTime? = null,
+    protected val updatedBy: String? = null
+)
