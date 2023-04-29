15643e2cfdae14782a1096c42eed688525defe6b
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-04-30 00:10:05 +0900

    fix: disable bootJar, jar

--- terms/terms-domain/build.gradle.kts
+++ terms/terms-domain/build.gradle.kts
@@ -1,14 +1,8 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 group = "com.microecommerce.terms.domain"
 version = "0.0.1"
 
 dependencies {
     implementation(project(":core"))
-}
-
-tasks.named<BootJar>("bootJar") {
-    enabled = false
 }
 
 tasks.named<Jar>("jar") {
