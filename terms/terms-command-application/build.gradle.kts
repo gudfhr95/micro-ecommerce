d546093931223ee8b14bb1ab3f478ab4441d1b5c
Hyungrok Ham <ghldnjsdlf@gmail.com>
2023-05-01 18:48:10 +0900

    chore: add terms command application

---
+++ terms/terms-command-application/build.gradle.kts
@@ -0,0 +1,11 @@
+group = "com.microecommerce.terms.application"
+version = "0.0.1"
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":terms:terms-domain"))
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
